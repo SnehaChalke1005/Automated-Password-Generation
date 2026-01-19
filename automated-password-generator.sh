f5a81f12fc8bba4d0000b0fb860cf5017d23c055
SnehaChalke1005 <chalkesneha1005@gmail.com>
2026-01-20 00:57:28 +0530

    Add automated password generator script
    This script creates a new user account with a randomly generated password and an optional comment. It checks for root privileges and handles errors during account creation and password assignment.

---
+++ automated-password-generator.sh
@@ -0,0 +1,57 @@
+
+
+#!/bin/bash
+
+# Root privileges.
+if [[ $UID -eq 0 ]]
+then
+  echo "WELCOME!!!"
+else
+  echo "YOU ARE NOT ROOT!!!"
+  exit 1
+fi
+
+echo
+
+if [[ "$#" -lt 1 ]]
+then
+  echo "With $0 supply your USER_NAME [COMMENT]."
+  echo "YOU CAN ALSO ADD SOME COMMENTS RELATED TO YOU ACCOUNT PLEASE ADD A COMMENT IN...'COMMENT'...IN THIS FORMAT(OPTIONAL)."
+  exit 1
+fi
+
+
+USER_NAME="$1"
+
+COMMENT="$2"
+
+PASSWORD="$RANDOM$(date +%s%N | sha256sum | head -c9)"
+
+SPECIAL_CHARACTER="$(echo '#$%^&*()' | fold -w1 | shuf | head -c1)"
+
+useradd -c "$COMMENT" -m $USER_NAME
+
+if [[ $? -ne 0 ]]
+then 
+  echo "THE ACCOUNT WAS NOT CREATED DUE TO SOME ISSUE...PLEASE TRY AGAIN \n SORRY FOR THE INCONVENIENCE. "
+  exit 1
+fi
+
+#echo $PASSWORD$SPECIAL_CHARACTER | passwd --stdin $USER_NAME
+echo "$USER_NAME:$PASSWORD$SPECIALCHARACTER" | chpasswd
+
+if [[ $? -ne 0 ]]
+then 
+  echo "YOUR PASSWORD WAS NOT ASSIGNED! PLEASE TRY AGAIN."
+  exit 1
+fi
+
+# DISPLAY
+
+echo
+echo "USER_NAME: $USER_NAME" 
+echo
+echo "PASSWORD: $PASSWORD" 
+echo
+
+exit 0
